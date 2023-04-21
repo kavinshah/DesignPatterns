bd05d9c38478fb8d3ebe4905f6bb6b802efe3bc3
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-04-21 11:42:43 -0400

    using lazy<T> fixing errors

--- Creational/Singleton/Singleton/AbstractFactory.cs
+++ Creational/Singleton/Singleton/AbstractFactory.cs
@@ -104,7 +104,6 @@ namespace AbstractFactory
         
         private DinosaurNoiseProducer()
         {
-            counter++;
         }
 
         public static IDinosaurNoiseProducer GetInstance()
