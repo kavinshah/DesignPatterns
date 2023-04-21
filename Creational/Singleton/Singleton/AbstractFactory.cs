7eaea32c90320738ef6a08ce01c3018f0bab058d
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-04-21 10:58:29 -0400

    basic lock approach

---
+++ Creational/Singleton/Singleton/AbstractFactory.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ObjectiveC;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+
+    public enum DinosaurType
+    {
+        TRex,
+        Stegosaurus
+    }
+
+    public interface IDinosaur
+    {
+        public void MakeANoise();
+    }
+
+    public interface IDinosaurFactory
+    {
+        public IDinosaur CreateADinosaur();
+    }
+
+    public interface IDinosaurNoiseProducer
+    {
+        public void MakeADinosaurNoise(IDinosaurFactory dinosaurFactory);
+    }
+
+    public class TRex : IDinosaur
+    {
+        public void MakeANoise()
+        {
+            Console.WriteLine("Making TRex Noise!!");
+        }
+    }
+
+    public class Stegosaurus : IDinosaur
+    {
+        public void MakeANoise()
+        {
+            Console.WriteLine("Making Stegosaurus Noise!!");
+        }
+    }
+
+    public class DinosaurFactory : IDinosaurFactory
+    {
+        private readonly DinosaurType dinosaurType;
+
+        public DinosaurFactory(string dinosaurName)
+        {
+            Enum.TryParse(dinosaurName, true, out this.dinosaurType);
+        }
+
+        public IDinosaur CreateADinosaur()
+        {
+            IDinosaur dinosaur=null;
+            switch (dinosaurType)
+            {
+                case DinosaurType.TRex:
+                    dinosaur = new TRex();
+                    break;
+                case DinosaurType.Stegosaurus:
+                    dinosaur = new Stegosaurus();
+                    break;
+                default:
+                    dinosaur = new TRex();
+                    break;
+            }
+            return dinosaur;
+        }
+    }
+
+    public class RandomDinosaurFactory : IDinosaurFactory
+    {
+        private Random random;
+        public IDinosaur dinosaur;
+        public RandomDinosaurFactory(Random random)
+        {
+            this.random = random;
+        }
+
+        public IDinosaur CreateADinosaur()
+        {
+            int randomNumber = random.Next(0, 10);
+
+            if (randomNumber < 5)
+            {
+                dinosaur = new TRex();
+            }
+            else
+            {
+                dinosaur = new Stegosaurus();
+            }
+            return dinosaur;
+        }
+    }
+
+    public class DinosaurNoiseProducer : IDinosaurNoiseProducer
+    {
+        private static IDinosaurNoiseProducer instance;
+        private static object lock_obj = new object();
+        private DinosaurNoiseProducer()
+        {
+        }
+
+        public static IDinosaurNoiseProducer GetInstance()
+        {
+            lock (lock_obj)
+                {
+                    if (instance == null)
+                    {
+                        instance = new DinosaurNoiseProducer();
+                    }
+                }
+            
+            return instance;
+        }
+
+        void IDinosaurNoiseProducer.MakeADinosaurNoise(IDinosaurFactory dinosaurFactory)
+        {
+            IDinosaur dinosaur = dinosaurFactory.CreateADinosaur();
+            dinosaur.MakeANoise();
+        }
+    }
+}
---
+++ Creational/Singleton/Singleton/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AbstractFactory;
+
+DinosaurNoiseProducer.GetInstance().MakeADinosaurNoise(new DinosaurFactory("TRex"));
+DinosaurNoiseProducer.GetInstance().MakeADinosaurNoise(new DinosaurFactory("Stegosaurus"));
+DinosaurNoiseProducer.GetInstance().MakeADinosaurNoise(new RandomDinosaurFactory(new Random()));
