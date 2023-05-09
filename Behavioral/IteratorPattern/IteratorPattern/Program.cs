e07bc698d04e31902d191c736a58c5a1b4289164
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-05-09 16:35:22 -0400

    iteratorPattern

---
+++ Behavioral/IteratorPattern/IteratorPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IteratorPattern;
+
+WordsCollection collection = new WordsCollection();
+collection.AddItem("Kavin");
+collection.AddItem("Shah");
+collection.AddItem("Sharad");
+
+foreach (var item in collection)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Using while loop");
+
+AlphabetIterator iterator = collection.GetEnumerator();
+while (iterator.MoveNext())
+{
+    Console.WriteLine(iterator.Current);
+}
+
