f306cb7f0161c10ee70c0ba387e6545f858394ad
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-11-03 13:02:47 +0530

    prototype example

---
+++ Creational/Prototype/Prototype/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Prototype;
+
+ColorManager colorManager = new ColorManager();
+colorManager["Red"] = new Color(255,0,0);
+colorManager["Green"] = new Color(0, 255, 0);
+colorManager["Blue"] = new Color(0, 0, 255);
+
+colorManager["angry"] = new Color(255, 54, 0);
+colorManager["peace"] = new Color(128, 211, 128);
+colorManager["flame"] = new Color(211, 34, 20);
+
+Color color1 = colorManager["Red"].Clone() as Color;
+Color color2 = colorManager["Green"].Clone() as Color;
+Color color3 = colorManager["Blue"].Clone() as Color;
+
+Console.WriteLine("\ncolor1");
+Console.WriteLine(color1);
+Console.WriteLine("\ncolor2");
+Console.WriteLine(color2);
+Console.WriteLine("\ncolor3");
+Console.WriteLine(color3);
+
---
+++ Creational/Prototype/Prototype/Prototype.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public abstract class ColorPrototype
+    {
+        public abstract ColorPrototype Clone();
+    }
+
+    public class Color : ColorPrototype
+    {
+        int red, green, blue;
+
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+        public override string ToString()
+        {
+            return "Red:" + this.red
+                + "\nGreen:" + this.green
+                + "\nBlue:" + this.blue;
+        }
+        public override ColorPrototype Clone()
+        {
+            return MemberwiseClone() as ColorPrototype;
+        }
+    }
+
+    public class ColorManager
+    {
+        private Dictionary<string, ColorPrototype> colors = new Dictionary<string, ColorPrototype> ();
+
+        public ColorPrototype this[string key]
+        {
+            get { return colors[key]; }
+            set { colors.Add(key, value); }
+        }
+    }
+}
