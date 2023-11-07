61d1dfabed5a250ca3626558e48eae469d45587a
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-11-07 10:11:50 +0530

    adding example of subclassing

--- Creational/Singleton/Singleton/Program.cs
+++ Creational/Singleton/Singleton/Program.cs
@@ -5,3 +5,40 @@ DinosaurNoiseProducer.GetInstance().MakeADinosaurNoise(new DinosaurFactory("TRex
 DinosaurNoiseProducer.GetInstance().MakeADinosaurNoise(new DinosaurFactory("Stegosaurus"));
 DinosaurNoiseProducer.GetInstance().MakeADinosaurNoise(new RandomDinosaurFactory(new Random()));
 //NewDinosaurNoiseProducer.GetInstance();
+
+/*
+ example of subclassing a singleton class.
+ */
+class A
+{
+    private A instance;
+    protected A()
+    {
+    }
+
+    public virtual A GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new A();
+        }
+        return instance;
+    }
+}
+
+class B: A
+{
+    private B instance;
+    protected B()
+    {
+    }
+
+    public override B GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new B();
+        }
+        return instance;
+    }
+}
