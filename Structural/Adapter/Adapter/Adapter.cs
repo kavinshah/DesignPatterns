7de7561072b8bfa4674a6c0ff66b2494e38f9777
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-11-09 22:31:54 +0530

    adapter pattern

---
+++ Structural/Adapter/Adapter/Adapter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    internal static class ChildCreator
+    {
+        internal static IChild CreateChild(IMammal mammal)
+        {
+            return mammal.GiveBirth();
+        }
+    }
+    
+    internal interface IChild
+    {
+        void Cry();
+    }
+
+    internal interface IMammal
+    {
+        IChild GiveBirth();
+    }
+
+    internal class Triceratops
+    {
+        public TriceratopsEggs LayEggs()
+        {
+            return new TriceratopsEggs();
+        }
+    }
+
+    internal class TriceratopsEggs
+    {
+        public IChild HatchEggs()
+        {
+            return new TriceratopsChild();
+        }
+    }
+
+    internal class TriceratopsChild : IChild
+    {
+        public TriceratopsChild() { }
+
+        public void Cry()
+        {
+            Console.WriteLine("TRICERATOPS IS CRYING!");
+        }
+    }
+
+    internal class TriceratopsToMammalAdapter : IMammal
+    {
+        Triceratops triceratops;
+        public TriceratopsToMammalAdapter(Triceratops triceratops)
+        {
+            this.triceratops = triceratops;
+        }
+        public IChild GiveBirth()
+        {
+            return triceratops?.LayEggs().HatchEggs();
+        }
+    }
+}
---
+++ Structural/Adapter/Adapter/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Adapter;
+
+IChild triceratopsChild = ChildCreator.CreateChild(new TriceratopsToMammalAdapter(new Triceratops()));
+triceratopsChild.Cry();
