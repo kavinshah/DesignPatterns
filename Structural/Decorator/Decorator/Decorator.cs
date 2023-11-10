5d0dba895304ae80cfc004d3b2f152db1c3045b3
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-11-10 20:47:52 +0530

    decorator

---
+++ Structural/Decorator/Decorator/Decorator.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    internal abstract class LibraryItem
+    {
+        private int numCopies;
+
+        public int NumCopies
+        {
+            get { return numCopies; }
+            set { numCopies = value; }
+        }
+
+        public abstract void Display();
+    }
+
+    internal class Book : LibraryItem
+    {
+        string author;
+        string title;
+        public Book(string author, string title, int numCopies)
+        {
+            this.author = author;
+            this.title = title;
+            this.NumCopies = numCopies;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("\nBook");
+            Console.WriteLine("Author: {0}", author);
+            Console.WriteLine("Title: {0}", title);
+            Console.WriteLine("#copies: {0}", NumCopies);
+        }
+    }
+
+    internal class Video: LibraryItem
+    {
+        string director;
+        string title;
+        int playtime;
+        
+        public Video(string director, string title, int playtime, int numofCopies)
+        {
+            this.director = director;
+            this.title = title;
+            this.playtime = playtime;
+            this.NumCopies = numofCopies;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("\nVideo");
+            Console.WriteLine("Director: {0}", director);
+            Console.WriteLine("Title: {0}", title);
+            Console.WriteLine("PlayTime: {0}", playtime);
+            Console.WriteLine("# Copies: {0}", NumCopies);
+        }
+
+        internal class Decorator: LibraryItem
+        {
+            LibraryItem item;
+            public Decorator(LibraryItem item)
+            {
+                this.item  = item;
+            }
+
+            public override void Display()
+            {
+                item.Display();
+            }
+        }
+
+        internal class Borrowable : Decorator
+        {
+            List<string> borrowers = new List<string>();
+            public Borrowable(LibraryItem item) : base(item)
+            {
+            }
+
+            public void Borrow(string name)
+            {
+                borrowers.Add(name);
+                NumCopies--;
+            }
+
+            public void Return(string name)
+            {
+                borrowers.Remove(name);
+                NumCopies++;
+            }
+
+            public override void Display()
+            {
+                base.Display();
+
+                foreach(string name in borrowers)
+                {
+                    Console.WriteLine("Borrower:" + name);
+                }
+            }
+        }
+
+    }
+}
---
+++ Structural/Decorator/Decorator/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Decorator;
+using static Decorator.Video;
+
+Book book = new Book("Morhan Housel", "The Psychology of Money", 20);
+book.Display();
+
+Video video = new Video("Some exotic director name", "Good Will Hunting", 120, 10);
+video.Display();
+
+Console.WriteLine("Making video borrowable");
+
+Borrowable borrowable = new Borrowable(video);
+borrowable.Borrow("Customer 1");
+borrowable.Borrow("Customer 2");
+borrowable.Display();
