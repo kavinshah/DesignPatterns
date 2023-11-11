168556a3a06d22b032008a282b071f3d1069e858
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-11-11 15:43:22 +0530

    proxy pattern

---
+++ Structural/Proxy/Proxy/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Proxy;
+
+INest secureNest = new SecureNestProxy(new RealNest());
+secureNest.Access("Kavin");
+
+secureNest.Access("TRex");
---
+++ Structural/Proxy/Proxy/Proxy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    internal interface INest
+    {
+        void Access(string name);
+    }
+
+    internal class RealNest: INest
+    {
+        public void Access(string name)
+        {
+            Console.WriteLine($"{name} has access to the nest");
+        }
+    }
+
+    internal class SecureNestProxy : INest
+    {
+        INest nest;
+        public SecureNestProxy(INest nest)
+        {
+            this.nest = nest;
+        }
+
+        public void Access(string name)
+        {
+            if(name.Equals("TRex") || name.Equals("Giganotosaurus"))
+            {
+                throw new UnauthorizedAccessException($"{name} is not allowed to access the nest.");
+            }
+
+            nest?.Access(name);
+        }
+    }
+
+
+    internal class Proxy
+    {
+
+    }
+}
