1cba3532739ab3e02fb7f2520a9fd05f79f2d8c6
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-11-13 11:50:38 +0530

    new composite example

---
+++ Structural/Composite/Composite 2/Composite.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite_2
+{
+    internal interface IPlant
+    {
+        void Eat();
+    }
+
+    internal class Leaf : IPlant
+    {
+        bool isEaten = false;
+        public Leaf() { }
+
+        public void Eat()
+        {
+            isEaten = true;
+            Console.WriteLine("A Leaf is eaten");
+        }
+    }
+
+    internal class Branch : IPlant
+    {
+        IList<IPlant> leaves;
+
+        public Branch(IList<IPlant> leaves)
+        {
+            this.leaves= leaves;
+        }
+
+        public void Eat()
+        {
+            foreach(IPlant leaf in leaves)
+            {
+                leaf.Eat();
+            }
+        }
+    }
+
+    internal class Composite
+    {
+        public void Run()
+        {
+            IPlant branch1 = new Branch(new List<IPlant>() { new Leaf(), new Leaf(), new Leaf(), new Leaf() });
+            
+            IPlant branch2_1 = new Branch(new List<IPlant>() { new Leaf(), new Leaf() });
+            IPlant branch2_2 = new Branch(new List<IPlant>() { new Leaf(), new Leaf(), new Leaf() });
+            IPlant branch2 = new Branch(new List<IPlant>() { branch2_1, branch2_2 });
+            
+            IPlant plant = new Branch(new List<IPlant>() { branch1, branch2 , new Leaf(), new Leaf()});
+
+            plant.Eat();
+        }
+
+    }
+}
