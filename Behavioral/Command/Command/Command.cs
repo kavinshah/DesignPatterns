6130fecdc9e32a76c21d05aa26c03311c7381fb2
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-11-16 14:18:06 +0530

    adding command pattern changes

---
+++ Behavioral/Command/Command/Command.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    internal interface ICommand
+    {
+        void Execute();
+    }
+
+    internal class ComplexCommand : ICommand
+    {
+        Receiver receiver;
+        public ComplexCommand(Receiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public void Execute()
+        {
+            receiver.DoSomething1();
+            //use the receiver in some form
+            receiver.DoSomething2();
+
+        }
+    }
+
+    internal class SimpleCommand : ICommand
+    {
+
+        public void Execute()
+        {
+            //use the receiver in some form
+            Console.WriteLine("Performing simple command");
+        }
+    }
+
+    internal class Receiver
+    {
+        public void DoSomething1()
+        {
+            Console.WriteLine("Doing something in method 1 in the receiver");
+        }
+
+        public void DoSomething2()
+        {
+            Console.WriteLine("Doing something in method 2 in the receiver");
+        }
+    }
+
+    internal class Invoker
+    {
+        ICommand onStart, onFinish;
+        public void OnStart(ICommand command)
+        {
+            this.onStart = command;
+        }
+
+        public void OnFinish(ICommand command)
+        {
+            this.onFinish = command;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Performing some action onstart");
+            if (onStart != null)
+                onStart.Execute();
+
+            Console.WriteLine("Performing some action onfinish");
+
+            if (onFinish != null)
+                onFinish.Execute();
+        }
+    }
+
+
+}
---
+++ Behavioral/Command/Command/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Command;
+
+Invoker invoker = new Invoker();
+invoker.OnStart(new SimpleCommand());
+invoker.OnFinish(new ComplexCommand(new Receiver()));
+invoker.Execute();
+
