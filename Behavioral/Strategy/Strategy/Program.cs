7c8be39188c8d3e5552c6034c46aaf3a2cd0d216
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-11-17 11:49:48 +0530

    strategy pattern

---
+++ Behavioral/Strategy/Strategy/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Strategy;
+
+Context context = new Context(new StrategyAdd());
+Console.WriteLine(context.Execute(10, 5));
+
+context = new Context(new StrategySubtract());
+Console.WriteLine(context.Execute(10, 5));
+
+context = new Context(new StrategyMultiply());
+Console.WriteLine(context.Execute(5, 10));
+
---
+++ Behavioral/Strategy/Strategy/Strategy.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    internal interface IStrategy
+    {
+        int PerformOperation(int a, int b);
+    }
+
+    internal class Context
+    {
+        IStrategy strategy;
+        public Context(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public int Execute(int a , int b)
+        {
+            return strategy.PerformOperation(a, b);
+        }
+    }
+
+    internal class StrategyAdd : IStrategy
+    {
+        public int PerformOperation(int a, int b)
+        {
+            int result;
+            result = a + b;
+            return result;
+        }
+    }
+
+    internal class StrategySubtract : IStrategy
+    {
+        public int PerformOperation(int a, int b)
+        {
+            int result;
+            result = a - b;
+            return result;
+        }
+    }
+
+    internal class StrategyMultiply : IStrategy
+    {
+        public int PerformOperation(int a, int b)
+        {
+            int result;
+            result = a * b;
+            return result;
+        }
+    }
+
+}
