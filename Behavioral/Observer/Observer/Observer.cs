062174180338f73c617af114f1fdd2713410ad28
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-11-19 13:21:52 +0530

    observer pattern

---
+++ Behavioral/Observer/Observer/Observer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    internal interface IObserver
+    {
+        public void Update(ISubject subject);
+    }
+
+    internal interface ISubject
+    {
+        int State { get; set; }
+        void Attach(IObserver observer);
+
+        void Detach(IObserver observer);
+
+        void Notify();
+    }
+
+    internal class Subject : ISubject
+    {
+        int status;
+        List<IObserver> listeners;
+
+        public int State
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public Subject()
+        {
+            listeners = new List<IObserver>();
+        }
+
+        public void Attach(IObserver oberver)
+        {
+            listeners.Add(oberver);
+        }
+
+        public void Detach(IObserver oberver)
+        {
+            listeners.Remove(oberver);
+        }
+
+        public void Notify() 
+        {
+            foreach(IObserver observer in listeners)
+            {
+                observer.Update(this);
+            }
+        }
+
+        public void SomeBusinessLogic()
+        {
+            Console.WriteLine("Performing some business logic");
+            State = Random.Shared.Next(10);
+            Thread.Sleep(1000);
+            Notify();
+
+        }
+    }
+
+    internal class ConcreteObserverA : IObserver 
+    {
+        public void Update(ISubject subject)
+        {
+            if(subject != null && subject.State < 3)
+            {
+                Console.WriteLine($"State value has change to {subject.State} in observerA");
+            }
+        }
+    }
+
+    internal class ConcreteObserverB : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            if (subject != null && (subject.State == 0 || subject.State > 3))
+            {
+                Console.WriteLine($"State value has change to {subject.State} observerB");
+            }
+        }
+    }
+
+
+}
---
+++ Behavioral/Observer/Observer/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Observer;
+
+Subject subject = new Subject();
+IObserver observer1 = new ConcreteObserverA();
+IObserver observer2 = new ConcreteObserverB();
+
+subject.Attach(observer1);
+subject.Attach(observer2);
+
+subject.SomeBusinessLogic();
+
+subject.Detach(observer2);
+
+subject.SomeBusinessLogic();
+
